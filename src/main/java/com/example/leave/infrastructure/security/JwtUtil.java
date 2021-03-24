d6c96297f76456f9866dafd4ec5ce3a406b3b582
muinme <phamanhtu242@gmail.com>
2021-03-24 08:49:45 +0700

    update

--- src/main/java/com/example/leave/infrastructure/security/JwtUtil.java
+++ src/main/java/com/example/leave/infrastructure/security/JwtUtil.java
@@ -1,12 +1,11 @@
 package com.example.leave.infrastructure.security;
 
-import com.example.leave.models.User;
 import io.jsonwebtoken.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -14,7 +13,7 @@ import java.util.Map;
 @Component
 @Slf4j
 public class JwtUtil {
-    private final String JWT_SECRET = "lodaaaaaa";
+    private static String JWT_SECRET = "lodaaaaaa";
 
     //Thời gian có hiệu lực của chuỗi jwt
     private final long JWT_EXPIRATION = 604800000L;
@@ -44,9 +43,26 @@ public class JwtUtil {
     }
 
     private String doGenerateToken(Map<String, Object> claims, String subject) {
-        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+        JwtBuilder builder = Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + JWT_EXPIRATION * 1000))
-                .signWith(SignatureAlgorithm.HS512, JWT_SECRET).compact();
+                .signWith(SignatureAlgorithm.HS512, JWT_SECRET);
+        String token = builder.compact();
+        RedisUtil.INSTANCE.sadd(JWT_SECRET, subject);
+
+        return token;
+    }
+    public static String parseToken(HttpServletRequest httpServletRequest, String jwtTokenCookieName, String signingKey){
+        String token = CookieUtil.getValue(httpServletRequest, jwtTokenCookieName);
+        if(token == null) {
+            return null;
+        }
+
+        String subject = Jwts.parser().setSigningKey(signingKey).parseClaimsJws(token).getBody().getSubject();
+        if (!RedisUtil.INSTANCE.sismember(JWT_SECRET, subject)) {
+            return null;
+        }
+
+        return subject;
     }
 
     public Boolean validateToken(String token, UserDetails userDetails){
